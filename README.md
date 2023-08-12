571437c498d9badd81709c50a92a00df844807cf
Eddy518 <edmwangi2@gmail.com>
2023-08-12 11:43:57 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+**MEMORY GAME**
+Memory game is a game where you have to match two cards on a board until none are left 
---
+++ index.js
@@ -0,0 +1,106 @@
+const cardsArray = [
+  {
+    name: "cheeseburger",
+    img: "images/cheeseburger.png",
+  },
+  {
+    name: "fries",
+    img: "images/fries.png",
+  },
+  {
+    name: "hotdog",
+    img: "images/hotdog.png",
+  },
+  {
+    name: "ice-cream",
+    img: "images/ice-cream.png",
+  },
+  {
+    name: "milkshake",
+    img: "images/milkshake.png",
+  },
+  {
+    name: "pizza",
+    img: "images/pizza.png",
+  },
+  {
+    name: "cheeseburger",
+    img: "images/cheeseburger.png",
+  },
+  {
+    name: "fries",
+    img: "images/fries.png",
+  },
+  {
+    name: "hotdog",
+    img: "images/hotdog.png",
+  },
+  {
+    name: "ice-cream",
+    img: "images/ice-cream.png",
+  },
+  {
+    name: "milkshake",
+    img: "images/milkshake.png",
+  },
+  {
+    name: "pizza",
+    img: "images/pizza.png",
+  },
+];
+cardsArray.sort(() => 0.5 - Math.random());
+
+const gridDisplay = document.querySelector("#grid");
+let cardsChosen = [];
+let cardsChosenIds = [];
+
+function cardBoard() {
+  for (let i = 0; i < cardsArray.length; i++) {
+    let card = document.createElement("img");
+    card.setAttribute("src", "images/blank.png");
+    card.setAttribute("data-id", i);
+    gridDisplay.appendChild(card);
+    card.addEventListener("click", flipCard);
+
+    console.log(card);
+  }
+}
+cardBoard();
+
+function checkMatch() {
+  const cards = document.querySelectorAll("#grid img");
+  console.log("Check a match");
+  console.log(cards);
+  //* Potential problem: User clicks on same card twice
+  if (cardsChosenIds[0] !== cardsChosenIds[1]) {
+    if (cardsChosen[0] === cardsChosen[1]) {
+      console.log(cards[cardsChosenIds[0]]);
+      console.log(cards[cardsChosenIds[1]]);
+      cards[cardsChosenIds[0]].classList.add("visibility");
+      cards[cardsChosenIds[1]].classList.add("visibility");
+      console.log("Match found");
+    }
+  }
+  cards[cardsChosenIds[0]].setAttribute("src", "images/blank.png");
+  cards[cardsChosenIds[1]].setAttribute("src", "images/blank.png");
+  cardsChosen = [];
+  cardsChosenIds = [];
+}
+
+function flipCard() {
+  // ? what happens when flipping a card
+  console.log(`When flipping card this is ${this}`);
+  let cardId = this.getAttribute("data-id"); // * GET DATA ID ON CURRENT CLICKED ELEMENT
+  cardsChosenIds.push(cardId);
+  cardsChosen.push(cardsArray[cardId].name);
+
+  this.setAttribute("src", cardsArray[cardId].img); // * CHANGE FROM BLANK TO $IMAGE
+
+  if (cardsChosen.length === 2) {
+    setTimeout(checkMatch, 500);
+  }
+
+  console.log(`cardId is ${cardId}`);
+  console.log(`cardsChosenId is ${cardsChosenIds}`);
+  console.log(`cardsChosen is ${cardsChosen}`);
+}
